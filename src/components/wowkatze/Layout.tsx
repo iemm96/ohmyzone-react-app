6746d5081417c2a36164ce103eb9861170478fb7
Emmanuel Fernández <iemm96@gmail.com>
2022-01-25 16:57:08 -0600

    Se agregaron categorias collares, pulseras y aretes

--- src/components/wowkatze/Layout.tsx
+++ src/components/wowkatze/Layout.tsx
@@ -4,6 +4,7 @@ import img1 from '../../assets/wowkatze/img1.jpg';
 import img2 from '../../assets/wowkatze/img2.jpg';
 import img3 from '../../assets/wowkatze/img3.jpg';
 import img4 from '../../assets/wowkatze/img4.jpg';
+import collarModeloLucy from '../../assets/wowkatze/collares/collar_modelo_lucy.jpg';
 
 import tiktok from '../../assets/wowkatze/tiktok.png';
 
@@ -28,31 +29,151 @@ const mainData:MainData = {
         backgroundSrc:BG
     },
     categories: [{
-        title:"Más vendidos",
+        title:"Collares",
         cards:[{
-            title:"Chocker Purple Agate",
-            description:"echo a mano por manos mexicanas con mucho cariño. Ágata natural morada envuelta artesanalmente con alambre chapado en oro.",
+            title:"Modelo Lucy",
+            description:"Hecho a mano con mucho cariño. Cuarzo electrificado y chapa de oro.",
             buttonText:"¡Ir a comprar!",
-            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+un+Chocker+Purple+Agate",
-            imageSrc:img1
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+un+collar+Lucy",
+            imageSrc:"https://res.cloudinary.com/nucleodev/image/upload/v1643140128/ohmyzone/wowkatze/collares/collar_modelo_lucy_fah4zm.jpg"
         },{
-            title:"Pendientes Ojitos llorones",
-            description:"Elegantes y únicos pendientes con ojitos que todo lo ven, representan luz, sabiduría y al espíritu. Elaborados por manos mexicanas con mucho cariño.",
+            title:"Modelo Amor de Júpiter",
+            description:"Que elegancia la Júpiter, collar elaborado a mano con mucho amor, ojo de tigre y acero inoxidable.",
             buttonText:"¡Ir a comprar!",
-            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+los+Pendientes+Ojitos+llorones",
-            imageSrc:img2
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+un+collar+Amor+de+Jupiter",
+            imageSrc:"https://res.cloudinary.com/nucleodev/image/upload/v1643141448/ohmyzone/wowkatze/collares/collar_modelo_amor_de_jupiter_vhkdla.jpg"
         },{
-            title:"Pendientes Atrapa Sueños",
-            description:"¡Protege tus sueños con estos hermosos pendientes!",
+            title:"Modelo Cristal Estelar",
+            description:"¡Protege tus sueños con este hermoso y mágico collar, amatista y chapa de oro!",
             buttonText:"¡Ir a comprar!",
-            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+los+Pendientes+atrapa+sueños",
-            imageSrc:img3
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+un+collar+Cristal+Estelar",
+            imageSrc:"https://res.cloudinary.com/nucleodev/image/upload/v1643141448/ohmyzone/wowkatze/collares/collar_modelo_cristal_estelar_k6kbxw.jpg"
         },{
-            title:"Sakura Perlas",
-            description:"Hermosos pendientes, elegantes y al mismo tiempo kawaii, elaborados a mano por manitas mexicanas kawaii.",
+            title:"Modelo Kira",
+            description:"Irradía tu propia luz con Kira, elaborado con mucho amor, chapa de oro.",
             buttonText:"¡Ir a comprar!",
-            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+los+Pendientes+de+Sakura+perlas",
-            imageSrc:img4
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+el+collar+modelo+Kira",
+            imageSrc:"https://res.cloudinary.com/nucleodev/image/upload/v1643141448/ohmyzone/wowkatze/collares/collar_modelo_halia_gythce.jpg"
+        },{
+            title:"Modelo Halia",
+            description:"Poderosa y hermosa como el mar, elaborado con mucho cariño, cuarzo electrificado azul y chapa de oro.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+el+collar+modelo+Halia",
+            imageSrc:"https://res.cloudinary.com/nucleodev/image/upload/v1643141447/ohmyzone/wowkatze/collares/collar_modelo_magia_marina_ydbgci.jpg"
+        },{
+            title:"Modelo Feeling love",
+            description:"Hecho con más amor del que imaginas, acero inoxidable.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+el+collar+modelo+Feeling+love",
+            imageSrc:"https://res.cloudinary.com/nucleodev/image/upload/v1643141448/ohmyzone/wowkatze/collares/collar_modelo_feeling_love_l7otaq.jpg"
+        },{
+            title:"Modelo Lucy de acero",
+            description:"Hecho a mano con mucho cariño. Cuarzo electrificado, chapa de rodio y acero inoxidable.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+el+collar+modelo+Lucy+de+acero",
+            imageSrc:"https://res.cloudinary.com/nucleodev/image/upload/v1643141447/ohmyzone/wowkatze/collares/collar_modelo_lucy_de_acero_aoqkh6.jpg"
+        },{
+            title:"Modelo Picos femeninos",
+            description:"Hecho a mano con mucho cariño. Acero inoxidable.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+el+collar+modelo+Picos+femeninos",
+            imageSrc:"https://res.cloudinary.com/nucleodev/image/upload/v1643141447/ohmyzone/wowkatze/collares/collar_modelo_picos_femeninos_x4ozur.jpg"
+        },{
+            title:"Modelo Solana",
+            description:"Hecho a mano con mucho cariño. Acero inoxidable, cuarzo electrificado y chapa de rodio.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+el+collar+modelo+Solana",
+            imageSrc:"https://res.cloudinary.com/nucleodev/image/upload/v1643141447/ohmyzone/wowkatze/collares/collar_modelo_solana_wwh5ba.jpg"
+        },{
+            title:"Modelo Chica lunar",
+            description:"Hecho a mano con mucho cariño. Chapa de oro.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+el+chocker+modelo+Chica+lunar",
+            imageSrc:"https://res.cloudinary.com/nucleodev/image/upload/v1643141447/ohmyzone/wowkatze/collares/chocker_modelo_chica_lunar_pahxjs.jpg"
+        },{
+            title:"Modelo Sirio",
+            description:"Hecho a mano con mucho cariño. Acero inoxidable y cristal.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+el+collar+modelo+Sirio",
+            imageSrc:"https://res.cloudinary.com/nucleodev/image/upload/v1643141447/ohmyzone/wowkatze/collares/collar_modelo_sirio_frxtz7.jpg"
+        }]
+    },{     
+        title: "Aretes",
+        cards:[{
+            title: "Modelo Meissa",
+            description:"Las estrellas mas brillantes de la constelación de Orion, para tus oidos. Hecho a mano y con amor, con Amatista y chapa de oro.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+los+aretes+Meissa",
+            imageSrc: "https://res.cloudinary.com/nucleodev/image/upload/v1643149748/ohmyzone/wowkatze/aretes/aretes_modelo_meissa_vxbmlt.jpg"
+        },{
+            title: "Modelo Naira",
+            description:"Protégete con estos lindos ojos. Chapa de rodio.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+los+aretes+Naira",
+            imageSrc: "https://res.cloudinary.com/nucleodev/image/upload/v1643149747/ohmyzone/wowkatze/aretes/aretes_modelo_naira_bvhoh6.jpg"
+        },{
+            title: "Modelo Adhara",
+            description:"La estrella más hermosa de la constelación Canis Major. Piedra de carburo de silicio y Chapa de oro.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+los+aretes+Adhara",
+            imageSrc: "https://res.cloudinary.com/nucleodev/image/upload/v1643149747/ohmyzone/wowkatze/aretes/aretes_modelo_adhara_epviap.jpg"
+        },{
+            title: "Modelo Neptuno",
+            description:"Luce como la chica espacial que eres con Neptuno en tus oidos. Cristal y Chapa de oro.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+los+aretes+Neptuno",
+            imageSrc: "https://res.cloudinary.com/nucleodev/image/upload/v1643149747/ohmyzone/wowkatze/aretes/aretes_modelo_neptuno_uf7fca.jpg"
+        },{
+            title: "Modelo Grumpy Neko",
+            description:"Kawaii pero con carácter, Grumpy Neko son para tí. Broquel en Chapa de oro, dije en aleación.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+los+aretes+Grumpy+Neko",
+            imageSrc: "https://res.cloudinary.com/nucleodev/image/upload/v1643149747/ohmyzone/wowkatze/aretes/aretes_modelo_grumpy_neko_aqindl.jpg"
+        },{
+            title: "Modelo Bianca",
+            description:"La luna más blanca y hermosa para tus oidos. Cuarzo rosa y Chapa de oro.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+los+aretes+Bianca",
+            imageSrc: "https://res.cloudinary.com/nucleodev/image/upload/v1643149747/ohmyzone/wowkatze/aretes/aretes_modelo_bianca_silp2o.jpg"
+        },{
+            title: "Modelo Kaonashi Ghibli",
+            description:"Si eres amante de las peliculas de studio Ghibli, Kaonashi son para tí. CBroquel en Chapa de oro y dije en aleación.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+los+aretes+Kaonashi+Ghibli",
+            imageSrc: "https://res.cloudinary.com/nucleodev/image/upload/v1643149747/ohmyzone/wowkatze/aretes/aretes_modelo_kaonashi_Ghibli_v3ztgk.jpg"
+        }]
+    },{
+        title: "Pulseras",
+        cards:[{
+            title: "Modelo Celeste",
+            description:"El brazalete más mágico de tu colección. Hecho a mano y con amor, con ágata congelada y chapa de oro.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+la+pulsera+Celeste",
+            imageSrc: "https://res.cloudinary.com/nucleodev/image/upload/v1643150821/ohmyzone/wowkatze/Pulseras/brazalete_modelo_celeste_xuutf8.jpg"
+        },{
+            title: "Modelo Vía lactea",
+            description:"Hecho a mano y con amor, con ágatas, ojo de tigre para júpiter y chapa de oro.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+la+pulsera+Via+lactea",
+            imageSrc: "https://res.cloudinary.com/nucleodev/image/upload/v1643150891/ohmyzone/wowkatze/Pulseras/pulsera_modelo_via_lactea_m10wng.jpg"
+        },{
+            title: "Modelo Estrella Llameante",
+            description:"Hecho a mano y con amor, con ágatas y chapa de oro.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+la+pulsera+Estrella+llameante",
+            imageSrc: "https://res.cloudinary.com/nucleodev/image/upload/v1643150823/ohmyzone/wowkatze/Pulseras/brazalete_modelo_nebulosa_estrella_llameante_sg2dyp.jpg"
+        },{
+            title: "Modelo Selene",
+            description:"Hecho a mano y con amor, con ágatas y chapa de oro.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+la+pulsera+Selene",
+            imageSrc: "https://res.cloudinary.com/nucleodev/image/upload/v1643150891/ohmyzone/wowkatze/Pulseras/pulsera_modelo_selene_eeqhjf.jpg"
+        },{
+            title: "Modelo Protégeme",
+            description:"Hecho a mano y con amor, con ojos turcos y chapa de oro.",
+            buttonText:"¡Ir a comprar!",
+            url: "https://api.whatsapp.com/send/?phone=523314317251&text=Hola,+me+gustaría+comprar+la+pulsera+Protegeme",
+            imageSrc: "https://res.cloudinary.com/nucleodev/image/upload/v1643150827/ohmyzone/wowkatze/Pulseras/modelo_protegeme_a8cbed.jpg"
         }]
     },{
         title:"Mis links",
@@ -63,7 +184,8 @@ const mainData:MainData = {
             url: "https://vm.tiktok.com/ZM8hVFEvm/",
             imageSrc:tiktok
         }]
-    }]
+    }
+]
 };
 
 const Layout = () => {
